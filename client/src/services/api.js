fb6c0aee08b62db4e285c3e22cbeb23bca42f9d2
JuberAalam <90861834+JuberAalam@users.noreply.github.com>
2026-02-11 22:12:24 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,7 @@
 import axios from "axios";
-
-/* ================= AXIOS INSTANCE ================= */
-
-// Use environment variable for backend URL
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "https://tasklist-crn1.onrender.com/api",
 });
-
-/* ================= REQUEST INTERCEPTOR ================= */
 API.interceptors.request.use(
   (req) => {
     const token = localStorage.getItem("token");
@@ -17,7 +11,6 @@ API.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-/* ================= RESPONSE INTERCEPTOR ================= */
 API.interceptors.response.use(
   (res) => res,
   (error) => {
@@ -29,20 +22,12 @@ API.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-
-/* ================= AUTHENTICATION APIS ================= */
 export const registerUser = (data) => API.post("/auth/register", data);
 export const loginUser = (data) => API.post("/auth/login", data);
-
-/* ================= PROFILE APIS ================= */
 export const fetchProfile = () => API.get("/users/me");
 export const updateProfile = (data) => API.put("/users/me", data);
-
-/* ================= TASK APIS ================= */
 export const getTasks = () => API.get("/tasks");
 export const createTask = (data) => API.post("/tasks", data);
 export const updateTask = (taskId, data) => API.put(`/tasks/${taskId}`, data);
 export const deleteTask = (taskId) => API.delete(`/tasks/${taskId}`);
-
-/* ================= EXPORT AXIOS INSTANCE ================= */
 export default API;
